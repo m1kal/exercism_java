8a15b98752fdc5554582cbf04a72927a74ffca2b
m1kal <mikal82@protonmail.com>
2019-03-01 08:31:08 +0100

    Secret-handshake v3

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,34 +1,16 @@
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.math.BigInteger;
-
-class Signal_ {
-  Signal s;
-  public Signal_(Signal s) {
-    this.s = s;
-  }
-  public boolean isOk(BigInteger x) {
-    return x.testBit(s.ordinal());
-  }
-}
 
 class HandshakeCalculator {
-
-    List<Signal> calculateHandshake(BigInteger number) {
-      List<Signal> signals = Stream.of(Signal.values())
-                                   .map(s->new Signal_(s))
-                                   .filter(x-> x.isOk(number))
-                                   .map(s->s.s)
-                                   .collect(Collectors.toList());
-      if (number.testBit(4))
-        Collections.reverse(signals);
-      return signals;
-    }
-
     List<Signal> calculateHandshake(int number) {
-      return calculateHandshake(BigInteger.valueOf(number));
+        List<Signal> signals = new ArrayList<>();
+        for (int i = 0; i < 4; i++)
+            if (((number >> i) % 2) == 1)
+                signals.add(Signal.values()[i]);
+        if (((number >> 4) % 2) == 1)
+            Collections.reverse(signals);
+        return signals;
     }
 }
 
