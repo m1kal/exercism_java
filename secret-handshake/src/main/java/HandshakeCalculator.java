4029f1834c0eb0212effe3c9ee08ea32742f5f63
m1kal <mikal82@protonmail.com>
2019-04-23 07:07:44 +0200

    Secret handshake v4

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -3,12 +3,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class HandshakeCalculator {
+    boolean bitTest(int number, int position) {
+      return (((number >> position) & 1) == 1);
+    }
+
     List<Signal> calculateHandshake(int number) {
         List<Signal> signals = new ArrayList<>();
         for (int i = 0; i < 4; i++)
-            if (((number >> i) % 2) == 1)
+            if (bitTest(number, i))
                 signals.add(Signal.values()[i]);
-        if (((number >> 4) % 2) == 1)
+        if (bitTest(number, 4))
             Collections.reverse(signals);
         return signals;
     }
